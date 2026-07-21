1fbc3c867ff868485b79b0de6efc9232047ccca2
atishraoon <satishoraon881@gmail.com>
2026-07-21 22:47:17 +0530

    optimize

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, 'core' ,'.env'))
 
-
+  
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, 'core' ,'.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True   
+DEBUG = os.environ.get("DEBUG") == "True"  
 
 ALLOWED_HOSTS = ['*','127.0.0.1','himanitourtravel.pythonanywhere.com']
 
